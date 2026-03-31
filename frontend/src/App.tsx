3a73d224780f0446ddcdfaa911c21dcafdc3ffe6
Nut-Natthawut <natthawutkaensao@gmail.com>
2026-03-31 22:36:29 +0700

    feat: replace dashboard redirect with new Homepage component in app routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { Toaster } from 'sonner';
 
@@ -9,13 +9,14 @@ import FormNew from './pages/FormNew';
 import FormView from './pages/FormView';
 import SearchPage from './pages/Search';
 import AdminUsersPage from './pages/AdminUsers';
+import Homepage from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+          <Route path='/' element={<Homepage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/form/new" element={<FormNew />} />
